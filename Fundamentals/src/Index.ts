f870c2cd05400c6c91d50cd6c7f599e5448c037c
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-04 01:50:16 +0400

    Objects.

--- Fundamentals/src/Index.ts
+++ Fundamentals/src/Index.ts
@@ -1,7 +1,10 @@
-function calculateTax(income: number, taxYear = 2022): number {
-    if (taxYear < 2022)
-        return income * 1.2;
-    return income * 1.3;
-}
-
-calculateTax(10_000, 2023);
+let employee: {
+    readonly id: number;
+    name: string;
+    retire: (date: Date) => void
+} = { 
+    id: 1, 
+    name: 'Hasmik',
+    retire: (date: Date) => 
+        console.log(date)
+};
