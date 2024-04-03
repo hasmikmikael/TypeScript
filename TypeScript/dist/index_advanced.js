f5098b7a161c8b0776a11c01067c0dfbb6cf6556
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-04 02:37:07 +0400

    Union Types.

--- TypeScript/dist/index_advanced.js
+++ TypeScript/dist/index_advanced.js
@@ -4,4 +4,12 @@ let empl = {
     name: 'Hasmik',
     retire: (date) => console.log(date)
 };
+function KgToLbs(weight) {
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+KgToLbs(10);
+KgToLbs('10kg');
 //# sourceMappingURL=index_advanced.js.map
--- TypeScript/src/index_advanced.ts
+++ TypeScript/src/index_advanced.ts
@@ -11,3 +11,15 @@ let empl: Employee = {
     retire: (date: Date) => 
         console.log(date)
 };
+
+//Union Types
+function KgToLbs(weight: number | string): number {
+    //Narrowing
+    if (typeof weight === 'number')
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+KgToLbs(10);
+KgToLbs('10kg');
