050365a91f5b90fe2909d8b4f7291c421d5b6f62
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-04 00:36:16 +0400

    Built-in Types.

--- Fundamentals/src/Index.ts
+++ Fundamentals/src/Index.ts
@@ -1,4 +1,8 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+// let sales: number = 123_456_789;
+// let course: string = "TypeScript";
+// let is_published: boolean = true;
+
+let sales = 123_456_789;
+let course = "TypeScript";
+let is_published = true;
+let level;
