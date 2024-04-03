3f64b7a22fb2ffbf1208a87f026527359f1b727c
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-04 00:38:19 +0400

    The any Type.

--- Fundamentals/src/Index.ts
+++ Fundamentals/src/Index.ts
@@ -5,4 +5,11 @@
 let sales = 123_456_789;
 let course = "TypeScript";
 let is_published = true;
+
 let level;
+level = 1;
+level = 'a';
+
+function render(document: any) {
+    console.log(document);
+}
