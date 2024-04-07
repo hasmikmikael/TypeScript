66732ccd100f7e5610dcd335f03dc7b1b4f2d809
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-08 02:14:53 +0400

    Creating Classes.

---
+++ TypeScript/src/index_object_oriented_programming.ts
@@ -0,0 +1,18 @@
+//Classes
+class Account {
+    id: number;
+    owner: string;
+    balance: number;
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    deposit(amount: number): void {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance += amount;
+    }
+}
