da9af6f48bd594e83d1bdc419f4f54abd363097d
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-08 21:28:17 +0400

    Static Members.

--- TypeScript/dist/index_object_oriented_programming.js
+++ TypeScript/dist/index_object_oriented_programming.js
@@ -25,4 +25,16 @@ class SeatAssignment {
 let seats = new SeatAssignment();
 seats.A1 = 'John';
 seats.A2 = 'Tim';
+class Ride {
+    start() { Ride.activeRides++; }
+    ;
+    stop() { Ride.activeRides--; }
+    ;
+}
+Ride.activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index_object_oriented_programming.js.map
