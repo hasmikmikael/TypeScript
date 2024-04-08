ec5a1352e2004ac0d41ce4b9a092a2cb3bcb2860
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-08 19:37:55 +0400

    Access Control Keywords.

--- TypeScript/dist/index_object_oriented_programming.js
+++ TypeScript/dist/index_object_oriented_programming.js
@@ -3,17 +3,20 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error("Invalid amount");
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Hasmik', 0);
 account.deposit(100);
-console.log(account.balance);
+console.log(account.getBalance());
 console.log(account);
 console.log(typeof account);
 console.log(account instanceof Account);
--- TypeScript/src/index_object_oriented_programming.ts
+++ TypeScript/src/index_object_oriented_programming.ts
@@ -2,26 +2,32 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickname?: string;
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
+
+    //private calculateTax() {}
 
     deposit(amount: number): void {
         if (amount <= 0)
             throw new Error("Invalid amount");
-        this.balance += amount;
+        this._balance += amount;
+    }
+
+    getBalance(): number {
+        return this._balance;
     }
 }
 
 //Objects
 let account = new Account(1, 'Hasmik', 0);
 account.deposit(100);
-console.log(account.balance);
+console.log(account.getBalance());
 console.log(account);
 console.log(typeof account);
 console.log(account instanceof Account);
