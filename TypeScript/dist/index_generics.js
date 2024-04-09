fe890ebf95bef5447bc94e65280e4319c9449139
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-09 22:53:29 +0400

    Understanding the Problem.

---
+++ TypeScript/dist/index_generics.js
@@ -0,0 +1,15 @@
+"use strict";
+class NumberKeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+class StringKeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+let pair1 = new StringKeyValuePair('1', 'Apple');
+//# sourceMappingURL=index_generics.js.map
---
+++ TypeScript/src/index_generics.ts
@@ -0,0 +1,10 @@
+//Understanding the Problem
+class NumberKeyValuePair {
+    constructor(public key: number, public value: string) {}
+}
+
+class StringKeyValuePair {
+    constructor(public key: string, public value: string) {}
+}
+
+let pair1 = new StringKeyValuePair('1', 'Apple');
