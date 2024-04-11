903e60a071afa7fe0b4052864e5e2b644e98eddd
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-12 03:11:55 +0400

    Decorators Exercises.

---
+++ TypeScript/src/index_decorators_exercises.ts
@@ -0,0 +1,16 @@
+// Exercise 1: Create a decorator for adding a sauce to Pizza instances:
+// @Sauce('pesto')
+// class Pizza {}
+
+// In the above example, all instances of the Pizza class should have a sauce property set 
+// to pesto. 
+
+// Solution 1
+function Sauce(sauce: string) {
+    return (constructor: Function) => {
+        constructor.prototype.sauce = sauce;
+    }
+}
+
+@Sauce('pesto')
+class Pizza {}
