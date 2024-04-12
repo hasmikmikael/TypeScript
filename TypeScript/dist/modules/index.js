c1c6c0ee23da71d148060e444440c15f5640520e
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-12 17:42:35 +0400

    Module Formats.

--- TypeScript/dist/modules/index.js
+++ TypeScript/dist/modules/index.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const shapes_1 = require("./shapes");
-let sphere = new shapes_1.Sphere(1);
+import { Sphere } from "./shapes";
+let sphere = new Sphere(1);
 console.log(sphere.radius);
 //# sourceMappingURL=index.js.map
--- TypeScript/dist/modules/shapes.js
+++ TypeScript/dist/modules/shapes.js
@@ -1,16 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Square = exports.Sphere = void 0;
-class Sphere {
+export class Sphere {
     constructor(radius) {
         this.radius = radius;
     }
 }
-exports.Sphere = Sphere;
-class Square {
+export class Square {
     constructor(width) {
         this.width = width;
     }
 }
-exports.Square = Square;
 //# sourceMappingURL=shapes.js.map
