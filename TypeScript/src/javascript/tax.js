7aeb1430c9ebee4e3a0a16746aae3cee9e84a2fb
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-12 19:51:52 +0400

    Creating Declaration Files.

--- TypeScript/src/javascript/tax.js
+++ TypeScript/src/javascript/tax.js
@@ -1,8 +1,14 @@
-/**
- * Calculates income tax.
- * @param {number} income - Net salary after expenses.
- * @returns {number}
- */
+// //---------Describing Types Using JSDoc---------
+// /**
+//  * Calculates income tax.
+//  * @param {number} income - Net salary after expenses.
+//  * @returns {number}
+//  */
+// export function calculateTax(income) {
+//     return income * .3;
+// }
+
+//---------Creating Declaration Files---------
 export function calculateTax(income) {
     return income * .3;
 }
