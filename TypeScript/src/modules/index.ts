ddbeef28358eb63c580820607a7f24f1ad9ae7a4
Hasmik Mikaelyan <Hasmik@wireless2020.com>
2024-04-12 18:50:34 +0400

    Wildcard Imports.

--- TypeScript/src/modules/index.ts
+++ TypeScript/src/modules/index.ts
@@ -5,3 +5,9 @@ console.log(sphere.radius);
 
 //------Default Exports--------
 import Store, { Format } from "./storage";
+
+//------Wildcard Imports-------------
+//import { Sphere, Square } from "./shapes";
+import * as Shapes from "./shapes";
+
+let square = new Shapes.Square(1);
